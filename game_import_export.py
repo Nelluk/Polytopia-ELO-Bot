7f7bcd45020f09c8de8df2f34335cc756347ae1b
Cullen Stevenson <cullenstev@gmail.com>
2018-09-02 14:37:08 -0400

    Game export error handling

--- game_import_export.py
+++ game_import_export.py
@@ -18,9 +18,8 @@ class GameIO_Cog:
 
     @commands.command(aliases=['gex', 'gameexport'])
     @commands.has_any_role(*helper_roles)
-    @commands.cooldown(1, 10, commands.BucketType.guild)
+    @commands.cooldown(1, 300, commands.BucketType.guild)
     async def game_export(self, ctx):
-        print('here1')
 
         with open('games_export.csv', mode='w') as export_file:
             game_writer = csv.writer(export_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
@@ -28,38 +27,44 @@ class GameIO_Cog:
             header = ['ID', 'Winner', 'Home', 'Away', 'Date', 'Home1', 'Home2', 'Home3', 'Home4', 'Home5', 'Away1', 'Away2', 'Away3', 'Away4', 'Away5']
             game_writer.writerow(header)
 
-            with ctx.message.channel.typing():
-                print('here2')
-                query = Game.select().where(Game.is_completed == 1)
-                for q in query:
-                    print('here3')
-                    row = [q.id, q.winner.name, q.home_team.name, q.away_team.name, str(q.date)]
+            query = Game.select().where(Game.is_completed == 1)
+            for q in query:
+                row = [q.id, q.winner.name, q.home_team.name, q.away_team.name, str(q.date)]
 
-                    pquery = Lineup.select().where(Lineup.game == q.id)
-                    home_players = []
-                    away_players = []
-                    for lineup in pquery:
-                        if lineup.team == q.home_team:
-                            home_players.append(lineup.player.discord_name)
-                        else:
-                            away_players.append(lineup.player.discord_name)
+                pquery = Lineup.select().where(Lineup.game == q.id)
+                home_players = []
+                away_players = []
+                for lineup in pquery:
+                    if lineup.team == q.home_team:
+                        home_players.append(lineup.player.discord_name)
+                    else:
+                        away_players.append(lineup.player.discord_name)
 
-                    home_players.extend([''] * (5 - len(home_players)))  # Pad list of players with extra blank entries so total length is 5
-                    away_players.extend([''] * (5 - len(away_players)))
-                    row += home_players + away_players
-                    game_writer.writerow(row)
+                home_players.extend([''] * (5 - len(home_players)))  # Pad list of players with extra blank entries so total length is 5
+                away_players.extend([''] * (5 - len(away_players)))
+                row += home_players + away_players
+                game_writer.writerow(row)
 
         pb = Pastebin(pastebin_api)
         pb_url = pb.create_paste_from_file(filepath='games_export.csv', api_paste_private=0, api_paste_expire_date='1D', api_paste_name='Polytopia Game Data')
         await ctx.send(f'Game data has been export to the following URL: {pb_url}')
 
-    @game_export.error
     async def game_export_handler(self, ctx, error):
         """A local Error Handler
         The global on_command_error will still be invoked after."""
 
         if isinstance(error, commands.CommandOnCooldown):
             await ctx.send(f'This command is on cooldown. Try again in {int(error.retry_after)} seconds.')
+            return
+        if isinstance(error, commands.CommandInvokeError):
+            await ctx.send(f'Error creating export file.')
+            # If bot is run as a system service the export file will be created by root, which can't be over-written if bot is later run as a user
+            # One fix would be to reconfigure system service to run as the user, but that is a bit complicated
+            return
+        await ctx.send(f'Unknown error')
+        logger.warn('Unknown error suppressed in game_export command')
+        # This error handler is overly simple and can't raise exceptions that it doesn't specifically handle. No way around it other than
+        # writing a full error handler class.
 
 
 def setup(bot):
