8b34ed0e76046b2a49bc1940ada582a22b16022b
Cullen Stevenson <cullenstev@gmail.com>
2018-10-25 14:40:01 -0400

    Better leaderboard rank string for unranked players

--- modules/games.py
+++ modules/games.py
@@ -355,7 +355,8 @@ class games():
                     member_stats.append((member.name, 0, '\u200b'))
                 else:
                     wins, losses = p[0].get_record()
-                    member_stats.append((f'{p[0].name}', p[0].elo, f'*({p[0].elo}, #{p[0].leaderboard_rank(date_cutoff=settings.date_cutoff)[0]})*'))
+                    rank_str = f', #{p[0].leaderboard_rank(date_cutoff=settings.date_cutoff)[0]}' if p[0].leaderboard_rank(date_cutoff=settings.date_cutoff)[0] else ''
+                    member_stats.append((f'{p[0].name}', p[0].elo, f'*({p[0].elo}{rank_str})*'))
 
             member_stats.sort(key=lambda tup: tup[1], reverse=True)     # sort the list descending by ELO
             members_sorted = [f'{x[0]} {x[2]}' for x in member_stats]    # create list of strings like Nelluk(1000)
