00cfc08f5355b608494b83745c936acea1c32618
Cullen Stevenson <cullenstev@gmail.com>
2018-12-02 17:45:07 -0500

    ping command now CCs message to game channels

--- modules/misc.py
+++ modules/misc.py
@@ -139,7 +139,10 @@ class misc:
             return await ctx.send(f'Message was not included. Example usage: `{ctx.prefix}ping 100 Here\'s a nice note`')
 
         player_mentions = [f'<@{l.player.discord_member.discord_id}>' for l in game.lineup]
-        await ctx.send(f'Message from {ctx.author.mention} regarding game {game.id} **{game.name}**:\n*{message}*\n{" ".join(player_mentions)}')
+        full_message = f'Message from {ctx.author.mention} regarding game {game.id} **{game.name}**:\n*{message}*'
+
+        await ctx.send(f'{full_message}\n{" ".join(player_mentions)}')
+        await game.update_squad_channels(ctx, message=full_message)
 
     @commands.command(aliases=['gex', 'gameexport'])
     @settings.is_mod_check()
--- modules/models.py
+++ modules/models.py
@@ -549,8 +549,9 @@ class Game(BaseModel):
         for gameside in self.gamesides:
             if gameside.team_chan:
                 await channels.update_squad_channel_name(ctx, channel_id=gameside.team_chan, game_id=self.id, game_name=self.name, team_name=gameside.team.name)
-            if message:
-                await channels.send_message_to_channel(ctx, channel_id=gameside.team_chan, message=message)
+
+                if message:
+                    await channels.send_message_to_channel(ctx, channel_id=gameside.team_chan, message=message)
 
     async def update_announcement(self, ctx):
         # Updates contents of new game announcement with updated game_embed card
