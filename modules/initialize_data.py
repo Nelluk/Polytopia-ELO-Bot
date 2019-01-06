f0c846f34852e7c29fe05f9622d93068fcca0fe0
Cullen Stevenson <cullenstev@gmail.com>
2019-01-06 15:55:00 -0500

    novas bug fix

--- modules/initialize_data.py
+++ modules/initialize_data.py
@@ -23,7 +23,7 @@ def initialize_data():
 
     tribe_list = ['Bardur', 'Imperius', 'Xin-Xi', 'Oumaji', 'Kickoo', 'Hoodrick', 'Luxidoor', 'Vengir', 'Zebasi', 'Ai-Mo', 'Quetzali', 'Aquarion', 'Elyrion', 'Yadakk']
 
-    for guild_id in [478571892832206869]:
+    for guild_id in [447883341463814144]:
         # 447883341463814144 = Polychampions
         # 478571892832206869 = Nelluk Test Server
 
