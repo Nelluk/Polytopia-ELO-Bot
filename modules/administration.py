2e73654faeb36828ca9df6dda59b558022c80ad2
Cullen Stevenson <cullenstev@gmail.com>
2019-02-08 16:13:36 -0500

    output for confirm command

--- modules/administration.py
+++ modules/administration.py
@@ -80,7 +80,7 @@ class administration:
         winning_game.declare_winner(winning_side=winning_game.winner, confirm=True)
 
         await post_win_messaging(ctx, winning_game)
-        await ctx.send(f'Confirmation of game {winning_game.id} concluded.')  # Added here to try to fix InterfaceError Cursor Closed - seems to fix if there is output at the end
+        await ctx.send(f'**Game {winning_game.id}** winner has been confirmed as **{winning_game.winner.name()}**')  # Added here to try to fix InterfaceError Cursor Closed - seems to fix if there is output at the end
 
     @commands.command(usage='game_id')
     async def rankset(self, ctx, game: PolyGame = None):
