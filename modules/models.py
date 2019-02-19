f227d637369bf74b509c559b69b2963282cf4a0d
Cullen Stevenson <cullenstev@gmail.com>
2019-02-19 14:42:55 -0500

    fix for very big game sides breaking embeds

--- modules/models.py
+++ modules/models.py
@@ -825,16 +825,17 @@ class Game(BaseModel):
             side_capacity = side.capacity()
             capacity += side_capacity[1]
             player_list = []
-            for player_lineup in side.ordered_player_list():
+            ordered_player_list = list(side.ordered_player_list())
+            for player_lineup in ordered_player_list:
                 player = player_lineup.player
                 players += 1
                 tribe_str = player_lineup.tribe.emoji if player_lineup.tribe else ''
                 team_str = player.team.emoji if player.team else ''
+                poly_id_str = f'\n`{player.discord_member.polytopia_id}`' if len(ordered_player_list) < 10 else ''  # to avoid hidding 1024 char limit on very big sides
                 logger.debug(f'Building embed for game {self.id} - player {player.id} {player.name} is associated with team {player.team} - team_str: {team_str}')
-                player_list.append(f'**{player.name}** ({player.elo}) {tribe_str} {team_str}\n`{player.discord_member.polytopia_id}`')
+                player_list.append(f'**{player.name}** ({player.elo}) {tribe_str} {team_str}{poly_id_str}')
             player_str = '\u200b' if not player_list else '\n'.join(player_list)
-            if len(player_str) > 1024:
-                logger.error(f'Embed field in pending embed is too long - truncating. Need a more elegant fix for this.')
+
             embed.add_field(name=f'__Side {side.position}__{side_name} *({side_capacity[0]}/{side_capacity[1]})*', value=player_str[:1024], inline=False)
 
         return embed, content_str
