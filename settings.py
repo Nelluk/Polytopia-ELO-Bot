30619d419e00f4ac84846e687277f7adae9e76eb
Cullen Stevenson <cullenstev@gmail.com>
2019-03-01 22:07:07 -0500

    settings tweak

--- settings.py
+++ settings.py
@@ -106,7 +106,7 @@ config = {'default':
                       'include_in_global_lb': True,
                       'bot_channels_private': [418175357137453058],  # 058 testchamber
                       'bot_channels_strict': [403724174532673536],  # 536 BotCommands
-                      'bot_channels': [403724174532673536, 511316081160355852, 511906353476927498],  # 498 unranked-games, 852 ranked-games
+                      'bot_channels': [403724174532673536, 511316081160355852, 511906353476927498, 396069729657421824],  # 498 unranked-games, 852 ranked-games, 824 multi-discussion
                       'newbie_message_channels': [396069729657421824, 413721247260868618],  # multi-discussion, #friend-codes
                       'ranked_game_channel': 511316081160355852,
                       'unranked_game_channel': 511906353476927498,
