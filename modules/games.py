8a3610aba67f626a2dd28ae805ef49df31d90875
Cullen Stevenson <cullenstev@gmail.com>
2019-03-21 20:26:42 -0400

    hide MIA members from team display

--- modules/games.py
+++ modules/games.py
@@ -257,7 +257,13 @@ class elo_games():
             if not team_role:
                 logger.error(f'Could not find matching role for team {team.name}')
                 continue
-            team_name_str = f'**{team.name}**   ({len(team_role.members)})'  # Show team name with number of members
+            counter = 0
+            mia_role = discord.utils.get(ctx.guild.roles, name='MIA')
+            for team_member in team_role.members:
+                if mia_role and mia_role in team_member.roles:
+                    continue
+                counter += 1
+            team_name_str = f'**{team.name}**   ({counter})'  # Show team name with number of members without MIA role
             wins, losses = team.get_record(alltime=alltime)
 
             elo = team.elo_alltime if alltime else team.elo
