910151af5cb6cc6de3df4682083f6840f8593a98
Cullen Stevenson <cullenstev@gmail.com>
2019-05-09 21:49:01 -0400

    tweak to game_export ordering

--- modules/utilities.py
+++ modules/utilities.py
@@ -103,17 +103,18 @@ def export_game_data():
     with open(filename, mode='w') as export_file:
         game_writer = csv.writer(export_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-        header = ['game_id', 'server', 'game_name', 'game_type', 'game_date', 'completed_timestamp', 'side_id', 'side_name', 'player_name', 'winner', 'player_elo', 'player_elo_change', 'squad_elo', 'squad_elo_change', 'tribe']
+        header = ['game_id', 'server', 'game_name', 'game_type', 'game_date', 'rank_unranked', 'completed_timestamp', 'side_id', 'side_name', 'player_name', 'winner', 'player_elo', 'player_elo_change', 'squad_elo', 'squad_elo_change', 'tribe']
         game_writer.writerow(header)
 
         query = models.Lineup.select().join(models.Game).where(
             (models.Game.is_confirmed == 1)
-        ).order_by(models.Lineup.gameside_id).order_by(models.Lineup.game_id)
+        ).order_by(models.Lineup.gameside_id).order_by(models.Lineup.game_id).limit(100)
 
         for q in query:
             is_winner = True if q.game.winner == q.gameside_id else False
+            ranked_status = 'Ranked' if q.game.is_ranked else 'Unranked'
             row = [q.game_id, settings.guild_setting(q.game.guild_id, 'display_name'), q.game.name, q.game.size_string(),
-                   str(q.game.date), str(q.game.completed_ts), q.gameside_id,
+                   ranked_status, str(q.game.date), str(q.game.completed_ts), q.gameside_id,
                    q.gameside.name(), q.player.name, is_winner, q.player.elo,
                    q.elo_change_player, q.gameside.squad_id if q.gameside.squad else '', q.gameside.squad.elo if q.gameside.squad else '',
                    q.tribe.tribe.name if q.tribe else '']
