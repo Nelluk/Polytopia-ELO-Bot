63a4ee79c8dd41566c425634a44d9b5d096f8d1d
Cullen Stevenson <cullenstev@gmail.com>
2019-05-18 14:48:02 -0400

    test of league_balance command

--- modules/models.py
+++ modules/models.py
@@ -595,7 +595,7 @@ class Player(BaseModel):
 
         if elo_list:
             # print(elo_list)
-            return int(statistics.median(elo_list)), player_games
+            return int(statistics.mean(elo_list)), player_games
 
         return 0, 0
 
