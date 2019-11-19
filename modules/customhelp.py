9439826e142983579d67877d820301ec7ca729a1
Cullen Stevenson <cullenstev@gmail.com>
2019-11-19 07:56:11 -0500

    minimal help command

--- modules/customhelp.py
+++ modules/customhelp.py
@@ -2,8 +2,8 @@
 from discord.ext import commands
 
 
-# class MyHelpCommand(commands.MinimalHelpCommand):
-class MyHelpCommand(commands.DefaultHelpCommand):
+class MyHelpCommand(commands.MinimalHelpCommand):
+# class MyHelpCommand(commands.DefaultHelpCommand):
     def get_command_signature(self, command):
         return '{0.clean_prefix}{1.qualified_name} {1.signature}'.format(self, command)
 
