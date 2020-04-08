bcde71c86380aacefdecc7291fb50ce3e65f5f39
Cullen Stevenson <cullenstev@gmail.com>
2020-04-08 11:14:38 -0400

    ban list change

--- settings.py
+++ settings.py
@@ -600,7 +600,7 @@ poly_id_ban_list = [
     'pKUaK61nd2BzNY65',  # BlueberryCraft#9080 (star hacker)
     'MvSRS2t5vWLUyyuu',  # Caesar Augustas Trajan
     'AfMDTSO3yareZN2E',  # Freeze
-    'qIqw1okeZZgaFpUL',  # Remalin (skre alt)
+    # 'qIqw1okeZZgaFpUL',  # Remalin (skre alt)
     # '815D2hK94mN7StoL',  # Skrealder
     'fOEjbnrzO9tg1QYT',  # Doggo#8422
     '8ZWg85d9PlogdY1H',  # Stupid#7043
