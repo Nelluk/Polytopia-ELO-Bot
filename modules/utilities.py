3eafeb6abcddb89da33575c5fc89132dfdf616ce
Cullen Stevenson <cullenstev@gmail.com>
2020-05-13 20:17:23 -0400

    changed comments in get_guild_member

--- modules/utilities.py
+++ modules/utilities.py
@@ -76,37 +76,40 @@ def string_to_user_id(input):
 
 async def get_guild_member(ctx, input):
 
-        # Find matching Guild member by @Mention or Name. Fall back to case-insensitive search
-        # TODO: support Username#Discriminator (ie an @mention without the @)
-        # TODO: use exceptions.NoSingleMatch etc like Player.get_or_except()
+    # Find matching Guild member by @Mention or Name. Fall back to case-insensitive search
+    # TODO: use exceptions.NoSingleMatch etc like Player.get_or_except()
+    # TODO: Work around flaw noted below in MemberConverter
 
-        guild_matches, substring_matches = [], []
-        try:
-            guild_matches.append(await commands.MemberConverter().convert(ctx, input))
-        except commands.errors.BadArgument:
-            pass
-            # No matches in standard MemberConverter. Move on to a case-insensitive search.
-            input = input.strip('@')  # Attempt to handle fake @Mentions that sometimes slip through
-            for p in ctx.guild.members:
-                name_str = p.nick.upper() + p.name.upper() if p.nick else p.name.upper()
-                if p.name.upper() == input.upper():
-                    guild_matches.append(p)
-                elif input.upper() in name_str:
-                    substring_matches.append(p)
+    guild_matches, substring_matches = [], []
+    try:
+        guild_matches.append(await commands.MemberConverter().convert(ctx, input))
+        # One flaw in MemberConverter is that if there are multiple exact matches, it will just return the first one
+        # (with no indication that there were more matches)
+        # Potential workaround is to only use MemberConverter if the input is a discord ID or includes a discriminator
+    except commands.errors.BadArgument:
+        pass
+        # No matches in standard MemberConverter. Move on to a case-insensitive search.
+        input = input.strip('@')  # Attempt to handle fake @Mentions that sometimes slip through
+        for p in ctx.guild.members:
+            name_str = p.nick.upper() + p.name.upper() if p.nick else p.name.upper()
+            if p.name.upper() == input.upper():
+                guild_matches.append(p)
+            elif input.upper() in name_str:
+                substring_matches.append(p)
 
-            return guild_matches + substring_matches
-            # if len(guild_matches) > 0:
-            #     return guild_matches
-            # if len(input) > 2:
-            #     return substring_matches
+        return guild_matches + substring_matches
+        # if len(guild_matches) > 0:
+        #     return guild_matches
+        # if len(input) > 2:
+        #     return substring_matches
 
-        return guild_matches
+    return guild_matches
 
 
 def get_matching_roles(discord_member, list_of_role_names):
-        # Given a Discord.Member and a ['List of', 'Role names'], return set of role names that the Member has.polytopia_id
-        member_roles = [x.name for x in discord_member.roles]
-        return set(member_roles).intersection(list_of_role_names)
+    # Given a Discord.Member and a ['List of', 'Role names'], return set of role names that the Member has.polytopia_id
+    member_roles = [x.name for x in discord_member.roles]
+    return set(member_roles).intersection(list_of_role_names)
 
 
 def summarize_game_list(games_query):
