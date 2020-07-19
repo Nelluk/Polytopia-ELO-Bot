087e6838d8d06d9e015aeb0bc6c77204a204edef
Cullen Stevenson <cullenstev@gmail.com>
2020-07-18 21:55:52 -0400

    roleelo accepts multiple roles and has pro and junior shortcuts

--- modules/league.py
+++ modules/league.py
@@ -685,7 +685,7 @@ class league(commands.Cog):
                 with open(filename, 'rb') as f:
                     file = io.BytesIO(f.read())
                 file = discord.File(file, filename=filename)
-                await ctx.send(f'Active players sorted by {sort_str} with any of the following roles: **{"/".join([r.name for r in roles])}**\nLoaded into a file `{filename}`, sorted by {sort_str}', file=file)
+                await ctx.send(f'Active players with any of the following roles: **{"/".join([r.name for r in roles])}**\nLoaded into a file `{filename}`, sorted by {sort_str}', file=file)
         else:
             await ctx.send(f'Listing {len(player_list)} active members with any of the following roles: **{"/".join([r.name for r in roles])}** (sorted by {sort_str})...')
             # without the escape then 'everyone.name' still is a mention
