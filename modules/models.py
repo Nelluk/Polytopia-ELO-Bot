01c33e627c92e420b87f4f45f4b52d19e81f8036
Cullen Stevenson <cullenstev@gmail.com>
2020-08-05 15:27:09 -0400

    message tweak for steam

--- modules/models.py
+++ modules/models.py
@@ -49,7 +49,7 @@ def is_registered_member():
         if ctx.invoked_with == 'help' and ctx.command.name != 'help':
             return False
         else:
-            await ctx.send(f'This command requires bot registration first. Type __`{ctx.prefix}setcode YOURCODEHERE`__ to get started.')
+            await ctx.send(f'This command requires bot registration first. Type __`{ctx.prefix}setcode YOURCODEHERE`__ or  __`{ctx.prefix}steamname Your Steam Username`__ to get started.')
         return False
     return commands.check(predicate)
 
