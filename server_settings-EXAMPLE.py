b3db640aad3f5afc5547d99c499ed030672406e3
Nelluk <mouser4@gmail.com>
2020-12-05 08:44:56 -0500

    updated server_settings.py example

--- server_settings-EXAMPLE.py
+++ server_settings-EXAMPLE.py
@@ -53,8 +53,8 @@ server_list = {'default':
                       'log_channel': 480078679930830849,
                       'game_channel_categories': [493149162238640161, 493149183155503105]},
         447883341463814144:                           # Polychampions
-                     {'helper_roles': ['Mod', 'Helper', 'ELO-Helper', 'Team Leader', 'Nova Coordinator'],
-                      'mod_roles': ['Mod'],
+                     {'mod_roles': ['Mod'],
+                      'helper_roles': ['Helper', 'League Helper', 'Mod', 'Team Leader', 'Nova Coordinator', 'Nova Helper'],
                       'user_roles_level_4': ['Team Co-Leader', 'ELO Hero', 'ELO Elite', 'ELO Master', 'ELO Titan', 'Event Organizer', 'Team Recruiter'],  # power user
                       'user_roles_level_3': ['@everyone'],  # power user
                       'user_roles_level_2': ['@everyone'],  # normal user
@@ -64,16 +64,17 @@ server_list = {'default':
                       'require_teams': True,
                       'allow_teams': True,
                       'allow_uneven_teams': True,
-                      'max_team_size': 6,
+                      'max_team_size': 7,
                       'command_prefix': '$',
                       'include_in_global_lb': True,
-                      'bot_channels_private': [469027618289614850, 531636068739710997, 447902433964851210, 627601961046507541, 721522509782057051, 448116504563810305],  # 986 elo-staff-talk, 850 dont-timeout, 997 novas, 210 s4-drafts
-                      'bot_channels_strict': [487303307224940545, 448317497473630229],  # 545 elo-commands, 229 bot-commands
-                      'bot_channels': [487303307224940545, 448317497473630229],
+                      'bot_channels_private': [469027618289614850, 531636068739710997, 447902433964851210, 627601961046507541, 721522509782057051, 448116504563810305, 742832436047511572, 448026188691013633, 476821513199157259, 447898174993203200, 447976656217571329, 447976715084627979, 473550562785493002, 447898121666822154, 447898222711799828, 630254768475865119, 447976872278753280, 459043002698956801, 750147746119417936],  # 986 elo-staff-talk, 850 dont-timeout, 997 novas, 210 s4-drafts
+                      'bot_channels_strict': [448317497473630229, 487303307224940545],  # 545 elo-commands, 229 bot-commands
+                      'bot_channels': [448317497473630229, 487303307224940545],
                       'ranked_game_channel': None,
                       'unranked_game_channel': None,
                       'match_challenge_channels': [],
                       'log_channel': 448116504563810305,  # just-bot-things
+                      'staff_help_channel': 742832436047511572,
                       'game_announce_channel': 487302138704429087,  # elo-drafts
                       'game_channel_categories': [488421911529914368, 514141474229846018, 519131761733795841, 550414365044637712, 563717211362164736, 568093671912636420, 574669105752440842, 689093537131790388]},  # elo-games-i, ii, iii, iv, v, vi, vii, viii
         283436219780825088:                           # Main Server
@@ -90,14 +91,16 @@ server_list = {'default':
                       'max_team_size': 2,
                       'command_prefix': '$',
                       'include_in_global_lb': True,
-                      'bot_channels_private': [418175357137453058, 403724174532673536],  # 058 testchamber, 536 bot-commands
+                      'bot_channels_private': [418175357137453058, 403724174532673536, 742857671237042176],  # 058 testchamber, 536 bot-commands
                       'bot_channels_strict': [635091071717867521],  # 521 elo-bot-commands
-                      'bot_channels': [635091071717867521, 403724174532673536, 511316081160355852, 511906353476927498],  # 498 unranked-games, 852 ranked-games
-                      'newbie_message_channels': [396069729657421824, 413721247260868618],  # multi-discussion, friend-codes
+                      'bot_channels': [635091071717867521, 403724174532673536, 511316081160355852, 511906353476927498, 736011613580034158],  # 498 unranked-games, 852 ranked-games
+                      'newbie_message_channels': [396069729657421824],  # multi-discussion
                       'ranked_game_channel': 511316081160355852,
                       'unranked_game_channel': 511906353476927498,
+                      'steam_game_channel': 736011613580034158,
                       'match_challenge_channels': [511316081160355852, 511906353476927498],
                       'log_channel': 418175357137453058,
+                      'staff_help_channel': 742857671237042176,
                       'game_announce_channel': 505523961812090900,
                       'game_channel_categories': [546527176380645395, 551747728548298758, 551748058690617354, 560104969580183562, 590592163751002124, 598599707148943361, 628288610235449364, 628288644729405452]}
 }
