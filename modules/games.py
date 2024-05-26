34c095d54842b29015249238b8ba37f431569947
Nelluk <cullenstev@gmail.com>
2024-05-26 17:39:23 -0400

    Tweak to task_purge_game_channels

--- modules/games.py
+++ modules/games.py
@@ -2119,12 +2119,9 @@ class polygames(commands.Cog):
             )
 
             logger.info(f'running task_purge_game_channels on {len(old_games)} games')
-            
-            season_games = Game.polychamps_season_games()[0]
-            # Doing one pre-emptive load of season games here so it isn't done once per season game per task run
 
             for game in old_games:
-                if game in season_games:
+                if game.league_season:
                     logger.debug(f'Skipping purge of game {game.id} since it is a season game')
                     continue
                 guild = self.bot.get_guild(game.guild_id)
